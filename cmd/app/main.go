741922ceb3cffd8849f02764074b401ce0a6f997
OskarCurzytek <oskarcurzytek@gmail.com>
2026-07-01 21:40:45 +0200

    websocket connection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"log"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
 func main() {
+	u := url.URL{Scheme: "wss", Host: "ws7.blitzortung.org", Path: "/"}
+
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+
+	defer conn.Close()
+
+	log.Println("connected")
+
+	conn.WriteMessage(websocket.TextMessage, []byte(`{"a":111}`))
+
+	for {
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+
+		log.Printf("type=%d, len=%d\n", msgType, len(msg))
+
+		log.Println("message:", string(msg))
+	}
 }
